c032d02afb4537290a843ed86708ad3093bb6eef
luoxiaozero <luoxiaozero@163.com>
2022-11-13 00:33:27 +0800

    🦄 refactor: 优化conversion_route逻辑

--- src/routing/conversion_router_mod.rs
+++ src/routing/conversion_router_mod.rs
@@ -9,34 +9,64 @@ pub fn conversion_router(router: Router) -> MatchRouter {
 }
 
 fn conversion_route_list(route_list: &Vec<Route>) -> Vec<MatchRoute> {
-    if route_list.len() == 0 {
+    if route_list.is_empty() {
         return vec![];
     }
+
     let mut not_params_list = Vec::<MatchRoute>::new();
     let mut params_list = Vec::<MatchRoute>::new();
+
     for route in route_list {
-        let path_split_list: Vec<&str> = route.1.split("/:").collect();
-        if path_split_list.len() == 1 {
-            not_params_list.push((format!(r"^{}$", path_split_list[0]), vec![], route.2));
+        let match_route = conversion_route(route);
+        if match_route.1.is_empty() {
+            not_params_list.push(match_route);
         } else {
-            let mut path_re = String::from("");
-            let mut path_params: Vec<String> = vec![];
-            for (index, path) in path_split_list.iter().enumerate() {
-                if index == 0 {
-                    path_re.push_str(path);
-                } else if let Some(index) = path.find('/') {
-                    path_params.push(path[..index].to_string());
-                    path_re.push_str(format!("{}{}", r"/([^/]*?)", &path[index..]).as_str());
-                } else {
-                    path_params.push(path.to_string());
-                    path_re.push_str(r"/([^/]*?)");
-                }
-            }
-            params_list.push((format!(r"^{}$", path_re), path_params, route.2));
+            params_list.push(match_route);
         }
     }
+
     not_params_list.sort_by(|a, b| b.0.cmp(&a.0));
     params_list.sort_by(|a, b| b.0.cmp(&a.0));
     not_params_list.append(&mut params_list);
     not_params_list
+}
+
+fn conversion_route(route: &Route) -> MatchRoute {
+    let path_split_list: Vec<&str> = route.1.split("/:").collect();
+    if path_split_list.len() == 1 {
+        (format!(r"^{}$", path_split_list[0]), vec![], route.2)
+    } else {
+        let mut path_re = String::from("");
+        let mut path_params: Vec<String> = vec![];
+        for (index, path) in path_split_list.iter().enumerate() {
+            if index == 0 {
+                path_re.push_str(path);
+            } else if let Some(index) = path.find('/') {
+                path_params.push(path[..index].to_string());
+                path_re.push_str(format!("{}{}", r"/([^/]*?)", &path[index..]).as_str());
+            } else {
+                path_params.push(path.to_string());
+                path_re.push_str(r"/([^/]*?)");
+            }
+        }
+        (format!(r"^{}$", path_re), path_params, route.2)
+    }
+}
+
+#[test]
+fn test_conversion_route_the_path() {
+    use crate::{request::HTTPMethod, Request, Response};
+
+    fn handle_index(_request: &Request) -> crate::Result<Response> {
+        Ok(Response::html_str("Hello Juri"))
+    }
+
+    let match_route = conversion_route(&(HTTPMethod::GET, "/aa".to_string(), handle_index));
+    assert_eq!(match_route.0, "^/aa$");
+
+    let match_route = conversion_route(&(HTTPMethod::GET, "/aa/:bb".to_string(), handle_index));
+    assert_eq!(match_route.0, "^/aa/([^/]*?)$");
+
+    let match_route = conversion_route(&(HTTPMethod::GET, "/aa/:bb/cc".to_string(), handle_index));
+    assert_eq!(match_route.0, "^/aa/([^/]*?)/cc$");
 }
