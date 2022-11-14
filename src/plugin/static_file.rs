82a7bb1033ec548a0feeafb2c9e33752e74f715b
luoxiao <luoxiaozero@163.com>
2022-11-14 13:37:03 +0800

    ✨ feat: 返回文件的MIME值

--- src/plugin/static_file.rs
+++ src/plugin/static_file.rs
@@ -1,5 +1,6 @@
-use std::{path::PathBuf, collections::HashMap};
 use crate::{request::HTTPMethod, routing::match_route_path, JuriPlugin, Request, Response};
+use regex::Regex;
+use std::{collections::HashMap, path::PathBuf};
 
 pub struct StaticFilePlugin {
     re_urls: Vec<String>,
@@ -19,8 +20,8 @@ impl StaticFilePlugin {
         for dir in self.dirs.iter() {
             if let Some(path) = dir.to_str() {
                 let file_path = PathBuf::from(format!("{}/{}", path, url_path));
-                if file_path.exists() {
-                    return Some(file_path)
+                if file_path.exists() && file_path.is_file() {
+                    return Some(file_path);
                 }
             }
         }
@@ -32,13 +33,32 @@ impl JuriPlugin for StaticFilePlugin {
     fn request(&self, request: &mut Request) -> Option<Response> {
         if request.method == HTTPMethod::GET {
             for re in self.re_urls.iter() {
-                if let Some(params_map) = match_route_path(re.to_string(), vec!["url_path".to_string()], request.path.clone()) {
+                if let Some(params_map) = match_route_path(
+                    re.to_string(),
+                    vec!["url_path".to_string()],
+                    request.path.clone(),
+                ) {
                     if let Some(url_path) = params_map.get("url_path") {
                         if let Some(file_path) = self.find_file_path(url_path) {
+                            let mut headers = HashMap::new();
+                            let re = Regex::new(r"\.(.*?)$").unwrap();
+                            if let Some(caps) = re.captures(&url_path) {
+                                if let Some(extension) = caps.get(1) {
+                                    if let Some(content_type) =
+                                        extension_to_mime(extension.as_str())
+                                    {
+                                        headers.insert(
+                                            "Content-Type".to_string(),
+                                            content_type.to_string(),
+                                        );
+                                    }
+                                }
+                            }
+
                             return Some(Response {
                                 status_code: 200,
-                                headers: HashMap::new(),
-                                body: crate::ResponseBody::Path(file_path)
+                                headers,
+                                body: crate::ResponseBody::Path(file_path),
                             });
                         }
                     }
@@ -49,11 +69,23 @@ impl JuriPlugin for StaticFilePlugin {
         None
     }
 
-    fn response(&self, _request: &Request, _response: &mut Response) {
-        
-    }
+    fn response(&self, _request: &Request, _response: &mut Response) {}
 }
 
+fn extension_to_mime(extension: &str) -> Option<&str> {
+    match extension {
+        "txt" => Some("text/plain"),
+        "html" => Some("text/html"),
+        "css" => Some("text/css"),
+        "js" => Some("text/javascript"),
+        
+        "png" => Some("image/png"),
+        "jpg" => Some("image/jpeg"),
+        "gif" => Some("image/gif"),
+        "svg" => Some("image/svg+xml"),
+        _ => None,
+    }
+}
 
 #[test]
 fn test_file_path() {
