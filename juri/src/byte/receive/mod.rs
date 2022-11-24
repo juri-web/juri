b8ac28a0afcdf78cd903a88e5f29acdf0ed44c15
luoxiaozero <luoxiaozero@163.com>
2022-11-24 13:18:40 +0800

    🦄 refactor: 读取 header 时换行分割

---
+++ juri/src/byte/receive/mod.rs
@@ -0,0 +1,112 @@
+use crate::{byte::stream::JuriStream, Config, Request};
+use async_std::{io::ReadExt, net::TcpStream};
+use std::sync::Arc;
+use std::time::Duration;
+mod newline;
+use self::newline::Newline;
+
+const BUFFER_SIZE: usize = 1024 * 2;
+
+async fn read_buffer(
+    stream: &mut TcpStream,
+    config: &Arc<Config>,
+) -> std::result::Result<(usize, Vec<u8>), crate::Error> {
+    let mut buffer = vec![0u8; BUFFER_SIZE];
+    let dur = Duration::from_secs(config.keep_alive_timeout);
+
+    let bytes_count = async_std::future::timeout(dur, async {
+        let bytes_count = stream.read(&mut buffer).await.map_err(|e| crate::Error {
+            code: 500,
+            reason: e.to_string(),
+        })?;
+        Ok(bytes_count)
+    })
+    .await
+    .map_err(|e| crate::Error {
+        code: 500,
+        reason: e.to_string(),
+    })??;
+
+    Ok((bytes_count, buffer))
+}
+
+pub async fn handle_bytes(
+    stream: &mut TcpStream,
+    config: &Arc<Config>,
+) -> std::result::Result<Request, crate::Error> {
+    // https://www.cnblogs.com/nxlhero/p/11670942.html
+    // https://rustcc.cn/article?id=2b7eb30b-61ae-4a3d-96fd-fc897ab7b1e0
+    let mut juri_stream = JuriStream::new();
+    let mut newline = Newline::new();
+
+    let is_read_body_finish = loop {
+        let (bytes_count, mut buffer) = read_buffer(stream, &config).await?;
+
+        if bytes_count == 0 {
+            break None;
+        } else {
+            newline.push(&mut buffer);
+
+            let is_exist_body = loop {
+                if let Some(header_bytes) = newline.next() {
+                    if header_bytes.is_empty() {
+                        break true;
+                    } 
+                    juri_stream.handle_request_header_bytes(header_bytes);
+                } else {
+                    break false;
+                }
+            };
+            
+            if is_exist_body {
+                if bytes_count < BUFFER_SIZE {
+                    break Some(true);
+                } else {
+                    break Some(false);
+                }
+            }
+        }
+        if bytes_count < BUFFER_SIZE {
+            break None;
+        }
+    };
+
+    juri_stream.header_end();
+
+    if let Some(is_read_body_finish) = is_read_body_finish {
+        if let Some(body_bytes) = &mut newline.get_residue_bytes() {
+            juri_stream.handle_request_body_bytes(body_bytes).await;
+        }
+        if !is_read_body_finish {
+            loop {
+                let (bytes_count, buffer) = read_buffer(stream, &config).await?;
+                if bytes_count == 0 {
+                    break;
+                } else {
+                    let body_bytes = &mut buffer[..bytes_count].to_vec();
+                    juri_stream.handle_request_body_bytes(body_bytes).await;
+                }
+                if bytes_count < BUFFER_SIZE {
+                    break;
+                }
+            }
+        }
+    } else if let Some(content_length) = juri_stream.header_map.get("Content-Length") {
+        // 处理读取 header 时，读取数据大小小于缓冲区大小，但是 header 已经读取完毕
+        if let Ok(_body_length) = content_length.parse::<usize>() {
+            loop {
+                let (bytes_count, buffer) = read_buffer(stream, &config).await?;
+                if bytes_count == 0 {
+                    break;
+                } else {
+                    let body_bytes = &mut buffer[..bytes_count].to_vec();
+                    juri_stream.handle_request_body_bytes(body_bytes).await;
+                }
+                if bytes_count < BUFFER_SIZE {
+                    break;
+                }
+            }
+        }
+    }
+    juri_stream.get_request()
+}
---
+++ juri/src/byte/receive/newline.rs
@@ -0,0 +1,114 @@
+use std::{iter::Iterator, vec};
+
+
+pub struct Newline {
+    bytes: Vec<u8>,
+    _marker: usize,
+}
+
+impl Newline {
+    pub fn new() -> Self {
+        Self {
+            bytes: vec![],
+            _marker: 0,
+        }
+    }
+
+    pub fn push(&mut self, bytes: &mut Vec<u8>) {
+        self.bytes.append(bytes);
+    }
+
+    pub fn get_residue_bytes(mut self) -> Option<Vec<u8>> {
+        if self.bytes.is_empty() {
+            None
+        } else {
+            let bytes = self.bytes.drain(..).collect();
+            self._marker = 0;
+            Some(bytes)
+        }
+    }
+}
+
+impl Iterator for Newline {
+    type Item = Vec<u8>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut flag_n = false;
+        let mut flag_r = false;
+
+        let mut bytes_iter = self.bytes[self._marker..].iter().enumerate();
+        loop {
+            if let Some((index, value)) = bytes_iter.next() {
+                if flag_r {
+                    if *value == 10 {
+                        flag_n = true;
+                    } else {
+                        flag_r = false;
+                    }
+                }
+                if *value == 13 {
+                    flag_r = true;
+                }
+
+                if flag_n && flag_r {
+                    let bytes: Vec<u8> = self.bytes.drain(..(index - 1)).collect();
+                    self.bytes.drain(..2);
+                    self._marker = 0;
+                    break Some(bytes);
+                }
+            } else {
+                self._marker = self.bytes.len();
+                if self.bytes.last() == Some(&13) {
+                    self._marker -= 1;
+                } 
+                break None;
+            }
+        }
+    }
+}
+
+#[test]
+fn test_newline() {
+    let mut line = Newline::new();
+    let mut bytes: Vec<u8> = vec![1, 2, 13, 10];
+    line.push(&mut bytes);
+
+    assert_eq!(line.next(), Some(vec![1, 2]));
+    assert_eq!(line.next(), None);
+    assert_eq!(line.get_residue_bytes(), None);
+
+    let mut line = Newline::new();
+    let mut bytes: Vec<u8> = vec![1, 2, 13, 10, 2, 3, 13, 10];
+    line.push(&mut bytes);
+
+    assert_eq!(line.next(), Some(vec![1, 2]));
+    assert_eq!(line.next(), Some(vec![2, 3]));
+    assert_eq!(line.next(), None);
+    assert_eq!(line.get_residue_bytes(), None);
+
+    let mut line = Newline::new();
+    let mut bytes: Vec<u8> = vec![1, 2, 13, 10, 2, 3, 13, 10, 13, 10];
+    line.push(&mut bytes);
+
+    assert_eq!(line.next(), Some(vec![1, 2]));
+    assert_eq!(line.next(), Some(vec![2, 3]));
+    assert_eq!(line.next(), Some(vec![]));
+    assert_eq!(line.next(), None);
+    assert_eq!(line.get_residue_bytes(), None);
+
+    let mut line = Newline::new();
+    let mut bytes: Vec<u8> = vec![1, 2, 13];
+    line.push(&mut bytes);
+
+    assert_eq!(line.next(), None);
+    let mut bytes: Vec<u8> = vec![10];
+    line.push(&mut bytes);
+    assert_eq!(line.next(), Some(vec![1, 2]));
+    assert_eq!(line.next(), None);
+    assert_eq!(line.get_residue_bytes(), None);
+
+    let mut line = Newline::new();
+    let mut bytes: Vec<u8> = vec![1, 2, 13, 10];
+    line.push(&mut bytes);
+
+}
