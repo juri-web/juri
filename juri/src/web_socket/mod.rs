53715950c819d33eb4dbd4a34a3968c1f924ac5e
luoxiaozero <luoxiaozero@163.com>
2022-11-26 19:35:35 +0800

    ✨ feat: 添加 WebScoket 类型判断功能

--- juri/src/web_socket/mod.rs
+++ juri/src/web_socket/mod.rs
@@ -1,17 +1,17 @@
 use crate::{response::IntoResponse, HTTPMethod, Request};
 mod stream;
+use futures_util::{future::BoxFuture, FutureExt};
 use std::future::Future;
 use stream::WebSocketStream;
 
-pub struct WebSocket<T> {
-    callback: Option<T>,
+type BoxWebSocketHandler =
+    Box<dyn FnOnce(WebSocketStream) -> BoxFuture<'static, ()> + Send + Sync + 'static>;
+
+pub struct WebSocket {
+    callback: Option<BoxWebSocketHandler>,
 }
 
-impl<T, Fut> WebSocket<T>
-where
-    T: FnOnce(WebSocketStream) -> Fut + Send + Sync + 'static,
-    Fut: Future + Send + 'static,
-{
+impl WebSocket {
     pub fn upgrader(request: &Request) -> crate::Result<Self> {
         if request.method != HTTPMethod::GET {
             Err(crate::Error {
@@ -43,21 +43,17 @@ where
     }
 }
 
-impl<T, Fut> WebSocket<T>
-where
-    T: FnOnce(WebSocketStream) -> Fut + Send + Sync + 'static,
-    Fut: Future + Send + 'static,
-{
-    pub fn on(&mut self, callback: T) {
-        self.callback = Some(callback);
+impl WebSocket {
+    pub fn on<F, Fut>(&mut self, callback: F)
+    where
+        F: FnOnce(WebSocketStream) -> Fut + Send + Sync + 'static,
+        Fut: Future + Send + 'static,
+    {
+        self.callback = Some(Box::new(|stream| (callback)(stream).map(|_| ()).boxed()));
     }
 }
 
-impl<T, Fut> IntoResponse for WebSocket<T>
-where
-    T: FnOnce(WebSocketStream) -> Fut + Send + Sync + 'static,
-    Fut: Future + Send + 'static,
-{
+impl IntoResponse for WebSocket {
     fn into_response(self) -> crate::Response {
         todo!()
     }
@@ -66,19 +62,58 @@ where
 #[cfg(test)]
 mod test {
     use super::WebSocket;
-    use crate::{response::IntoResponse, Request, Response};
+    use crate::{response::IntoResponse, Request};
+    use std::any::{Any, TypeId};
 
-    fn test_handle_success(request: &Request) -> crate::Result<Response> {
+    fn test_handle_success(request: &Request) -> crate::Result<impl IntoResponse> {
         let mut ws = WebSocket::upgrader(&request).unwrap();
 
         ws.on(|_stream| async {});
 
-        Ok(ws.into_response())
+        Ok(ws)
     }
 
     #[test]
-    fn test_success() {
-        let request = Request::new();
-        let _ = test_handle_success(&request);
+    fn test_success_type_id() {
+        let mut request = Request::new();
+
+        request.protocol_and_version = "HTTP/1.1".to_string();
+
+        request
+            .header_map
+            .insert("Connection".to_string(), "Upgrade".to_string());
+        request
+            .header_map
+            .insert("Upgrade".to_string(), "websocket".to_string());
+
+        let into = test_handle_success(&request).unwrap();
+        println!(
+            "{:?} {:?} {:?}",
+            TypeId::of::<WebSocket>(),
+            into.type_id(),
+            TypeId::of::<dyn IntoResponse>()
+        );
+    }
+
+    #[test]
+    fn test_success_type() {
+        let mut request = Request::new();
+
+        request.protocol_and_version = "HTTP/1.1".to_string();
+
+        request
+            .header_map
+            .insert("Connection".to_string(), "Upgrade".to_string());
+        request
+            .header_map
+            .insert("Upgrade".to_string(), "websocket".to_string());
+
+        let into = test_handle_success(&request).unwrap();
+
+        if let Some(_ws) = <dyn Any>::downcast_ref::<WebSocket>(&into) {
+            println!("is WebSocket");
+        } else {
+            println!("not WebSocket");
+        }
     }
 }
