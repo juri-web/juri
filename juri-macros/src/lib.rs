0844041219d80563b198ff6b29600cb9f5de8471
luoxiaozero <luoxiaozero@163.com>
2022-12-06 07:48:49 +0800

    ✨ feat(macros): 添加生成 handle

--- juri-macros/src/lib.rs
+++ juri-macros/src/lib.rs
@@ -2,7 +2,8 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 use quote::ToTokens;
-use syn::{parse_macro_input, AttributeArgs, Item};
+use syn::{parse_macro_input, AttributeArgs, Item, ItemFn, Result};
+mod utils;
 
 #[proc_macro_attribute]
 pub fn get(attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -58,7 +59,7 @@ pub fn post(attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut string = path.to_string();
     string = string[1..string.len() - 1].to_string();
 
-    let input = parse_macro_input!(item as Item);
+    let input = parse_macro_input!(item as ItemFn);
     let token_stream = input.to_token_stream();
 
     let mut token = token_stream.into_iter();
@@ -82,7 +83,11 @@ pub fn post(attr: TokenStream, item: TokenStream) -> TokenStream {
         new_token_stream = quote!(
             pub fn #fn_name() -> juri::Route {
                 #input
-                (juri::HTTPMethod::POST, #string.to_string(), #fn_name)
+                juri::Route {
+                    method: juri::HTTPMethod::POST,
+                    path: #string.to_string(),
+                    handle
+                }
             }
         )
         .into();
@@ -96,4 +101,18 @@ pub fn post(attr: TokenStream, item: TokenStream) -> TokenStream {
         .into();
     }
     new_token_stream
+}
+
+
+fn generate_struct(input: TokenStream) -> Result<TokenStream> {
+    let crate_name = utils::get_crate_name(false);
+    let item_fn = syn::parse::<ItemFn>(input)?;
+    let vis = item_fn.vis;
+
+    // let def_struct = quote! {
+
+    // }
+    let expanded = quote! {
+    };
+    Ok(expanded.into())
 }
