99eb3192c18758ce55b2b47429e41fc5cb37f7c9
luoxiaozero <luoxiaozero@163.com>
2022-12-24 18:07:18 +0800

    ✨ feat: StaticFilePlugin 初始化修改

--- juri/src/plugin/static_file.rs
+++ juri/src/plugin/static_file.rs
@@ -3,26 +3,28 @@ use regex::Regex;
 use std::{collections::HashMap, path::PathBuf};
 
 pub struct StaticFilePlugin {
-    re_urls: Vec<String>,
-    dirs: Vec<PathBuf>,
+    config: HashMap<String, Vec<PathBuf>>,
 }
 
 impl StaticFilePlugin {
-    pub fn new(urls: Vec<String>, dirs: Vec<PathBuf>) -> Self {
-        let mut re_urls = vec![];
-        for url in urls.iter() {
-            re_urls.push(format!("{}{}", url, r"/(.+)"));
-        }
-        StaticFilePlugin { re_urls, dirs }
+    pub fn new(mut config: HashMap<&str, Vec<PathBuf>>) -> Self {
+        let config = config
+            .drain()
+            .map(|(url, dirs)| (format!("{}{}", url, r"/(.+)"), dirs))
+            .collect();
+
+        StaticFilePlugin { config }
     }
 
-    fn find_file_path(&self, url_path: &String) -> Option<PathBuf> {
-        for dir in self.dirs.iter() {
-            if let Some(path) = dir.to_str() {
-                let file_path = PathBuf::from(format!("{}/{}", path, url_path));
-                if file_path.exists() && file_path.is_file() {
-                    return Some(file_path);
-                }
+    fn find_file_path(&self, re_url: &String, url_path: &String) -> Option<PathBuf> {
+        let dirs = self.config.get(re_url)?;
+        for dir in dirs.iter() {
+            let Some(dir) = dir.to_str() else {
+                continue;
+            };
+            let file_path = PathBuf::from(format!("{}/{}", dir, url_path));
+            if file_path.exists() && file_path.is_file() {
+                return Some(file_path);
             }
         }
         None
@@ -32,14 +34,14 @@ impl StaticFilePlugin {
 impl JuriPlugin for StaticFilePlugin {
     fn request(&self, request: &mut Request) -> Option<Response> {
         if request.method == HTTPMethod::GET {
-            for re in self.re_urls.iter() {
+            for re_url in self.config.keys() {
                 if let Some(params_map) = match_route_params(
-                    re.to_string(),
+                    re_url.clone(),
                     vec!["url_path".to_string()],
                     request.path.clone(),
                 ) {
                     if let Some(url_path) = params_map.get("url_path") {
-                        if let Some(file_path) = self.find_file_path(url_path) {
+                        if let Some(file_path) = self.find_file_path(re_url, url_path) {
                             let mut headers = HashMap::new();
                             let re = Regex::new(r"\.(.*?)$").unwrap();
                             if let Some(caps) = re.captures(&url_path) {
@@ -72,13 +74,15 @@ impl JuriPlugin for StaticFilePlugin {
     fn response(&self, _request: &Request, _response: &mut Response) {}
 }
 
+
+/// https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/MIME_types
 fn extension_to_mime(extension: &str) -> Option<&str> {
     match extension {
         "txt" => Some("text/plain"),
         "html" => Some("text/html"),
         "css" => Some("text/css"),
         "js" => Some("text/javascript"),
-        
+
         "png" => Some("image/png"),
         "jpg" => Some("image/jpeg"),
         "gif" => Some("image/gif"),
